cfc78e882010ba467df6387d8b1cbecd8d42667b
user <mariiaantoniv@gmail.com>
2025-06-11 00:25:10 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,111 @@
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { TodoList } from './components/TodoList';
+import { useState } from 'react';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
 
+export const App = () => {
+  const [todos, setTodos] = useState<Todo[]>(todosFromServer);
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setHasTitleError] = useState(false);
+
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [hasNameError, setHasNameError] = useState(false);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setHasTitleError(false);
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const user =
+      usersFromServer.find(us => us.id === Number(event.target.value)) || null;
+
+    setSelectedUser(user);
+    setHasNameError(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setHasTitleError(!title);
+    setHasNameError(!selectedUser);
+
+    if (!title || !selectedUser) {
+      return;
+    }
+
+    const newTodo = {
+      id: Math.max(...todos.map(todo => todo.id), 0) + 1,
+      title: title.replace(/[^a-zA-Z0-9а-яА-Я ]/g, '').trim(),
+      userId: selectedUser.id,
+      completed: false,
+      user: selectedUser,
+    };
+
+    setTodos([...todos, newTodo]);
+    setTitle('');
+    setSelectedUser('');
+    setHasNameError(false);
+    setHasTitleError(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>Add todo form</h1>
+
+      <form action="/api/todos" method="POST" onSubmit={handleSubmit}>
+        <div className="field">
+          <label htmlFor="title">Title:</label>
+
+          <input
+            id="title"
+            type="text"
+            data-cy="titleInput"
+            value={title}
+            onChange={handleTitleChange}
+            placeholder="Enter a title"
+          />
+
+          {hasTitleError && <span className="error">Please enter a title</span>}
+        </div>
+
+        <div className="field">
+          <label htmlFor="userSelect">User:</label>
+          <select
+            id="userSelect"
+            value={selectedUser?.id || ''}
+            data-cy="userSelect"
+            onChange={handleNameChange}
+          >
+            <option value="" disabled>
+              Choose a user
+            </option>
+
+            {usersFromServer.map(user => (
+              <option value={user.id} key={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+
+          {hasNameError && <span className="error">Please choose a user</span>}
+        </div>
+
+        <button type="submit" data-cy="submitButton">
+          Add
+        </button>
+      </form>
+
+      <TodoList todos={todos} />
+    </div>
+  );
+};
+
+/*
 export const App = () => {
   return (
     <div className="App">
@@ -59,3 +162,4 @@ export const App = () => {
     </div>
   );
 };
+ */
